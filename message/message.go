c06c7cc2e174bc5c672882c139f72c2ccfa5672a
shend <shendahui@gmail.com>
2023-08-16 09:43:07 +0800

    .

--- message/message.go
+++ message/message.go
@@ -1,6 +1,9 @@
 package message
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/jart/gosip/sip"
 )
 
@@ -61,4 +64,49 @@ func (m *Message) Clone() Message {
 
 func (m *Message) GetBranch() string {
 	return m.Msg.Via.Param.Get("branch").Value
+}
+
+func (m *Message) GetCallID() string {
+	return m.Msg.CallID
+}
+
+func (m *Message) GetToTag() string {
+	toParam := m.Msg.To.Param
+	if toParam == nil {
+		return ""
+	}
+	return toParam.Get("tag").Value
+}
+
+func (m *Message) GetFromTag() string {
+	fromParam := m.Msg.From.Param
+	if fromParam == nil {
+		return ""
+	}
+	return fromParam.Get("tag").Value
+}
+
+// MakeDialogIDFromMessage creates dialog ID of message.
+// returns error if CallID or To tag or From tag does not exist
+func (m *Message) MakeDialogIDFromMessage() (string, error) {
+	callID := m.GetCallID()
+	if callID == "" {
+		return "", fmt.Errorf("missing Call-ID header")
+	}
+
+	toTag := m.GetToTag()
+	if toTag == "" {
+		return "", fmt.Errorf("missing tag param in To header")
+	}
+
+	fromTag := m.GetFromTag()
+	if fromTag == "" {
+		return "", fmt.Errorf("missing tag param in From header")
+	}
+
+	return MakeDialogID(callID, toTag, fromTag), nil
+}
+
+func MakeDialogID(callID, innerID, externalID string) string {
+	return strings.Join([]string{callID, innerID, externalID}, "__")
 }
--- response.go
+++ response.go
@@ -6,6 +6,9 @@ func NewResponseFromRequest(req *message.Message, status int, phrase string) *me
 	res := req.Clone()
 	res.Msg.Status = status
 	res.Msg.Phrase = phrase
+	if req.GetToTag() == "" {
+		res.Msg.To.Tag()
+	}
 	res.Destination, res.Source = res.Source, res.Destination
 	return &res
 }
